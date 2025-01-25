44b6429993ea2c94a6c4e56b37b423fca85abd39
LeonardoAbFlo <marceloaf191928@gmail.com>
2025-01-24 21:54:29 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -25,7 +25,7 @@ size_z = st.sidebar.number_input("Tamaño Z", value=20.0)
 exhaustiveness = st.sidebar.slider("Exhaustividad", min_value=1, max_value=16, value=8)
 
 # Ruta absoluta del ejecutable de AutoDock Vina
-vina_executable = ""
+vina_executable = "Molecular-Docking/vina.exe"
 
 # Botón para ejecutar docking
 if st.button("Ejecutar Docking"):
